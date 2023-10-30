32b2d0926fdf29abe9bc906fab6ce2bc1852e526
Slava Logos <logos.slava@gmail.com>
2023-10-30 14:12:06 +0300

    cleanup unused component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 import { fetchCurrentDateNeoFeed } from "@/api/getAsteroids";
-import { Home as HomePage } from "./home/HomePage";
+import { CartWidget } from "@/app/components/CartWidget/CartWidget";
+import { DistanceUnitAsteroidList } from "@/app/components/DistanceUnitAsteroidList";
 
 async function HomeWrapper() {
-  const neoFeed = await fetchCurrentDateNeoFeed();
-  return <HomePage asteroids={neoFeed.near_earth_objects} />;
+  const { near_earth_objects } = await fetchCurrentDateNeoFeed();
+
+  return (
+    <div className="home-wrapper">
+      <section id="asteroid-date-list">
+        <h2 className="asteroid-list-title">Ближайшие подлёты астероидов</h2>
+        <DistanceUnitAsteroidList initialAsteroids={near_earth_objects} />
+      </section>
+
+      <CartWidget />
+    </div>
+  )
 }
 
 export default HomeWrapper;
