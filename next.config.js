4d6ee03688f6d5f2a306eb9e2bed732ea566f55d
Slava Logos <logos.slava@gmail.com>
2023-10-30 14:32:56 +0300

    use environment variable for access control header

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
                 source: "/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "http://localhost:3000" },
+                    { key: "Access-Control-Allow-Origin", value: process.env.ORIGIN_FRONT },
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
